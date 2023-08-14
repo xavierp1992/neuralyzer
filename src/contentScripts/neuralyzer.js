7c4ead3f44edbc4782b55fba03dfe7dea6aca7c9
xavierpsh <xavierpsh@spdigital.sg>
2023-08-14 17:04:56 +0800

    fix outdated codes

--- src/contentScripts/neuralyzer.js
+++ src/contentScripts/neuralyzer.js
@@ -1,7 +1,11 @@
 import './neuralyzer.css';
 import { OPTION_KEYS } from '../constants';
 import { createDot } from './dot';
+import { subscribeStatus } from './status';
 
 chrome.storage.sync.get(OPTION_KEYS, function (options) {
   document.body.appendChild(createDot(options.url));
+  if (options.statusUrl) {
+    subscribeStatus(options);
+  }
 });
